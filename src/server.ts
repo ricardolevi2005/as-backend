fde8e9dbe0189f0373a5cdda4bb9d973141845a9
Ricardo Levi <ricardolevi2005@hotmail.com>
2023-12-05 12:59:43 -0300

    create `site.ts` and `requestInterceptor.ts` files and update `server.ts`

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import express from "express";
 import cors from "cors";
 import https from "https";
 import http from "http";
+import siteRoutes from "./routes/site";
+import { requestInterceptor } from "./utils/requestInterceptor";
 
 const app = express();
 
@@ -10,18 +12,24 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.all("*", requestInterceptor);
 
-const runServer = (port:number, server:http.Server) => {
-    server.listen(port, () => {
-        console.log(`🚀 Running on PORT ${port}`);
-    });
-}
+// app.use('/admin', adminRoutes)
+app.use("/", siteRoutes);
+
+const runServer = (port: number, server: http.Server) => {
+  server.listen(port, () => {
+    console.log(`🚀 Running on PORT ${port}`);
+  });
+};
 
 const regularServer = http.createServer(app);
-if (process.env.NODE_ENV === 'production') {
-    // TODO: configurar SSL
-    // TODO: rodar server na 80 e na 443
-}else {
-    const serverPort: number = process.env.PORT ? parseInt(process.env.PORT) : 9000;
-    runServer(serverPort,regularServer);
+if (process.env.NODE_ENV === "production") {
+  // TODO: configurar SSL
+  // TODO: rodar server na 80 e na 443
+} else {
+  const serverPort: number = process.env.PORT
+    ? parseInt(process.env.PORT)
+    : 9000;
+  runServer(serverPort, regularServer);
 }
