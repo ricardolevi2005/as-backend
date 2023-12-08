0fbd117722dc5e078f3c9d24db5148e1eb46e622
Ricardo Levi <ricardolevi2005@hotmail.com>
2023-12-08 08:43:40 -0300

    Create `auth.ts` in controllers and update `admin.ts`

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -16,8 +16,18 @@ export const login: RequestHandler = (req, res) => {
   }
 
   res.json({ token: auth.createToken() });
-  
-  
-  
-  // Retorno da requisição
+};
+
+export const validate: RequestHandler = (req, res, next) => {
+  if (!req.headers.authorization) {
+    return res.status(403).json({ error: "Acesso negado" });
+  }
+
+  const token = req.headers.authorization.split(" ")[1];
+
+  if (!auth.validateToken(token)) {
+    return res.status(403).json({ error: "Acesso negado" });
+  }
+
+  next();
 };
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -3,7 +3,10 @@ import * as auth from "../controllers/auth";
 
 const router = Router();
 
-router.get("/ping", (req, res) => res.json({ pong: true }));
 router.post("/login", auth.login);
+
+router.get("/ping", auth.validate , (req, res) => res.json({ pong: true , admin: true }));
+
+router.get("/events", auth.validate , )
 
 export default router;
