838e3a72df4bbb640f7e7d82997d91ba7d8df21a
Ricardo Levi <ricardolevi2005@hotmail.com>
2023-12-11 17:58:11 -0300

    Finalization CRUD `groups`

---
+++ src/controllers/groups.ts
@@ -0,0 +1,80 @@
+import { RequestHandler } from "express";
+import * as groups from "../services/groups";
+import { z } from "zod";
+import { group } from "console";
+
+export const getAll: RequestHandler = async (req, res) => {
+  const { id_event } = req.params;
+
+  const items = await groups.getAll(parseInt(id_event));
+
+  if (items) return res.json({ groups: items });
+
+  res.json({ error: "Ocorreu um erro" });
+};
+
+export const getGroup: RequestHandler = async (req, res) => {
+  const { id, id_event } = req.params;
+
+  const groupItem = await groups.getOne({
+    id: parseInt(id),
+    id_event: parseInt(id_event),
+  });
+  if (groupItem) {
+    return res.json({ group: groupItem });
+  }
+
+  res.json({ error: "Ocorreu um erro" });
+};
+
+export const addGroup: RequestHandler = async (req, res) => {
+  const { id_event } = req.params;
+  const addGroupSchema = z.object({
+    name: z.string(),
+  });
+  const body = addGroupSchema.safeParse(req.body);
+  if (!body.success) {
+    return res.json({ error: "Dados Inválidos" });
+  }
+
+  const newGroup = await groups.add({
+    name: body.data.name,
+    id_event: parseInt(id_event),
+  });
+
+  if (newGroup) return res.status(201).json({ group: newGroup });
+
+  res.json({ error: "Ocorreu um erro" });
+};
+
+export const updateGroup: RequestHandler = async (req, res) => {
+  const { id, id_event } = req.params;
+  const updateGroupSchema = z.object({
+    name: z.string().optional(),
+  });
+  const body = updateGroupSchema.safeParse(req.body);
+  if (!body.success) return res.json({ error: "Dados Inválidos" });
+
+  const updatedGroup = await groups.update(
+    {
+      id: parseInt(id),
+      id_event: parseInt(id_event),
+    },
+    body.data
+  );
+  if (updatedGroup) return res.json({ group: updatedGroup });
+
+  res.json({ error: "Ocorreu um erro" });
+};
+
+export const removeGroup: RequestHandler = async (req, res) => {
+  const { id, id_event } = req.params;
+
+  const removedGroup = await groups.remove({
+    id: parseInt(id),
+    id_event: parseInt(id_event),
+  });
+  if (removedGroup) return res.json({ groups: removedGroup });
+
+  res.json({ error: "Ocorreu um erro" });
+};
--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import * as auth from "../controllers/auth";
 import * as events from "../controllers/events";
+import * as groups from "../controllers/groups";
 
 const router = Router();
 
@@ -12,8 +13,14 @@ router.get("/ping", auth.validate, (req, res) =>
 
 router.get("/events", auth.validate, events.getAll);
 router.get("/events/:id", auth.validate, events.getEvent);
-router.post("/events", auth.validate, events.addEvent)
+router.post("/events", auth.validate, events.addEvent);
 router.put("/events/:id", auth.validate, events.updateEvent);
 router.delete("/events/:id", auth.validate, events.removeEvent);
+
+router.get("/events/:id_event/groups", auth.validate, groups.getAll);
+router.get("/events/:id_event/groups/:id", auth.validate, groups.getGroup);
+router.post("/events/:id_event/groups", auth.validate, groups.addGroup)
+router.put("/events/:id_event/groups/:id", auth.validate, groups.updateGroup);
+router.delete("/events/:id_event/groups/:id", auth.validate, groups.removeGroup);
 
 export default router;
