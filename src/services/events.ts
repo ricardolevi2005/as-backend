b4dbd0c237b5460f77d45c69ed93aa88c9b6735a
Ricardo Levi <ricardolevi2005@hotmail.com>
2023-12-12 13:41:25 -0300

    Update services`events.ts`and create `match.ts` file

--- src/services/events.ts
+++ src/services/events.ts
@@ -1,4 +1,7 @@
 import { PrismaClient, Prisma } from "@prisma/client";
+import * as people from "./people";
+import * as groups from "./groups";
+import { encryptMatch } from "../utils/match";
 
 const prisma = new PrismaClient();
 
@@ -28,27 +31,111 @@ export const add = async (data: EventsCreateData) => {
   }
 };
 
-
 type EventsUpdateData = Prisma.Args<typeof prisma.event, "update">["data"];
 
-export const update = async (id:number , data: EventsUpdateData) => {
+export const update = async (id: number, data: EventsUpdateData) => {
   try {
-    return await prisma.event.update({ where: {id}, data });
+    return await prisma.event.update({ where: { id }, data });
   } catch (error) {
     return false;
   }
-}
+};
 
-export const remove = async (id:number) => {
+export const remove = async (id: number) => {
   try {
-    return await prisma.event.delete({ where: {id} });
+    return await prisma.event.delete({ where: { id } });
   } catch (error) {
     return false;
   }
-}
+};
 
-export const doMatches = async (id: number) : Promise<boolean> => {
+/*
+  ID DO EVENTO : 1
+  - Grupo A(id:3)
+  --Boniek
+  --Márcio
+  --Pedro
+
+  - Grupo B (id:4)
+  -- João
+  -- Inácio
 
 
-  return true; //temporário
-}
+  Grupo C (id:5)
+  --Gabriela
+  */
+
+export const doMatches = async (id: number): Promise<boolean> => {
+  const eventItem = await prisma.event.findFirst({
+    where: { id },
+    select: { grouped: true },
+  });
+
+  if (eventItem) {
+    const peopleList = await people.getAll({ id_event: id });
+    if (peopleList) {
+      let sortedList: { id: number; match: number }[] = [];
+      let sortable: number[] = [];
+
+      let attempts = 0;
+      let maxAttempts = peopleList.length;
+      let keepTrying = true;
+      while (keepTrying && attempts < maxAttempts) {
+        keepTrying = false;
+        attempts++;
+        sortedList = [];
+        sortable = peopleList.map((item) => item.id);
+
+        for (let i in peopleList) {
+          let sortableFiltered: number[] = sortable;
+          if (eventItem.grouped) {
+            sortableFiltered = sortable.filter((sortableItem) => {
+              let sortablePerson = peopleList.find(
+                (item) => item.id === sortableItem
+              );
+              return peopleList[i].id_group !== sortablePerson?.id_group;
+            });
+          }
+          if (
+            sortableFiltered.length === 0 ||
+            (sortableFiltered.length === 1 &&
+              peopleList[i].id === sortableFiltered[0])
+          ) {
+            keepTrying: true;
+          } else {
+            let sortedIndex = Math.floor(
+              Math.random() * sortableFiltered.length
+            );
+            while (sortableFiltered[sortedIndex] === peopleList[i].id) {
+              let sortedIndex = Math.floor(
+                Math.random() * sortableFiltered.length
+              );
+            }
+
+            sortedList.push({
+              id: peopleList[i].id,
+              match: sortableFiltered[sortedIndex],
+            });
+            sortable = sortable.filter(
+              (item) => item !== sortableFiltered[sortedIndex]
+            );
+          }
+        }
+      }
+
+      if (attempts < maxAttempts) {
+        for (let i in sortedList) {
+          await people.update(
+            {
+              id: sortedList[i].id,
+              id_event: id,
+            },
+            { matched: encryptMatch(sortedList[i].match) }
+          );
+        }
+        return true;
+      }
+    }
+  }
+  return false; //temporário
+};
